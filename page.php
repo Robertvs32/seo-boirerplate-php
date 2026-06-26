8a9469039de5a181a43f965fbf07fa9c223cb810
Robert <vs.robert4@gmail.com>
2026-06-26 02:45:04 -0300

    Esqueleto PHP - SEO

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+    $titulo = "";
+    $descricao = "";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $descricao?>">
+    <title><?php echo $titulo ?></title>
+</head>
+<body>
+    <!-- COMPONENTES -->
+</body>
+</html>
